3ebe7b327ad0cdf5fdede727aafc5eab7467dcd1
ap51 <alexander.privaloff@gmail.com>
2020-09-27 23:40:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,163 +1,108 @@
-const axios = require('axios');
-const jsonwebtoken = require('jsonwebtoken');
-
-const express = require('express');
-
-/* class Auth0rize {
-    constructor({ url, key, secret }) {
-        this.axios = axios.create({
-            baseURL: url
-        });
-
-        this.key = key;
-        this.secret = secret;
-    }
-
-    bots() {
-        return async (req, res, next) => {
-            let { data: { content } } = await this.axios({
-                url: '/service/bots',
-                method: 'POST',
-                data: {
-                    owner: this.key
-                }
-            });
-        
-            res.json(content);
-        }
-    }
-
-    link() {
-        return async (req, res, next) => {
-            let { bot_id, platform = 'web', meta = {} } = req.body;
-        
-            let data = {
-                key: this.key,
-                bot_id,
-                platform,
-                meta
-            }
-        
-            let jwt = jsonwebtoken.sign(data, this.secret);
-        
-            let { data: { content } } = await this.axios({
-                url: '/service/links',
-                method: 'GET',
-                params: { jwt }
-            });
-
-            res.json(content);
-        }
-    }
-
-    auth(callback) {
-        return async (req, res, next) => {
-            let { jwt } = req.body;
-        
-            let payload = {};
-
-            try {
-                payload = jsonwebtoken.verify(jwt, this.secret);
-            }
-            catch(error) {
-                payload.error = error;
-            }
-        
-            let { user, meta, token, error } = payload;
-
-            let message = void 0;
-
-            if(error) {
-                message = callback(req, res, next, error);
-            }
-            else {
-                let data = { token, user, meta };
-
-                message = callback(req, res, next, void 0, data);
-            }
-
-            message = message || 'HI';
-
-            res.json({ message });
-        }
-    }
-}
-
-const auth0rize = ({ url, key, secret, onAuth, end_points = { bots: '/bots', link: '/link', auth: '/auth' } }) => {
-    const app = express();
-
-    const instance = new Auth0rize({ url, key, secret });
-
-    const router = express.Router();
-
-    router.use(express.json());
-    router.use(express.urlencoded({ extended: true }));
-
-    router.use((req, res, next) => {
-        Object.setPrototypeOf(req, app.request);
-        Object.setPrototypeOf(res, app.response);
-    
-        req.res = res;
-        res.req = req;
-    
-        next();
-    });
-
-    router.get(end_points.bots || '/bots', instance.bots());
-    router.post(end_points.link || '/link', instance.link());
-    
-    router.post(end_points.auth || '/auth', instance.auth(onAuth));
-
-    return router;
-} */
-
 let _class = void 0;
 
 if(typeof(window) === 'undefined') {
     //server side
 
     const axios = require('axios');
-    class Auth0rize {
-        constructor({ api_key, http_client = axios, processSignIn }) {
-            this.tokens = {};
+    const jsonwebtoken = require('jsonwebtoken');
 
+    class Auth0rize {
+        constructor({ url, api_key, secret, onMessage, onSignIn }) {
             this.api_key = api_key;
-            this.processSignIn = processSignIn;
+            this.secret = secret;
+
+            this.onSignIn = onSignIn;
+            this.onMessage = onMessage;
+
+            this.auth0rize = axios.create({ baseURL: url });
 
             const SSEChannel = require('sse-pubsub');
             this.channel = new SSEChannel();
+
+            //setInterval(() => this.channel.publish(`test data ${Date.now()}`, 'interval'), 1000);
         }
 
-        middlewares(app) {
-            app.get('/sse', this.sse);
-            app.post('/apply', this.apply);
-            app.get('/sources', this.sources);
-            app.post('/create', this.create);
-            app.post('/signIn', this.signIn);
+        get middlewares() {
+            return [
+                (req, res, next) => {
+                    req.method === 'GET' && req.url.endsWith('/auth0rize.events') ? this.sse(req, res) : next();
+                },
+                (req, res, next) => {
+                    req.method === 'POST' && req.url.endsWith('/auth0rize.message') ? this.message(req, res) : next();
+                },
+                (req, res, next) => {
+                    req.method === 'GET' && req.url.endsWith('/auth0rize.sources') ? this.sources(req, res) : next();
+                },
+                (req, res, next) => {
+                    req.method === 'POST' && req.url.endsWith('/auth0rize.request') ? this.request(req, res) : next();
+                },
+                (req, res, next) => {
+                    req.method === 'POST' && req.url.endsWith('/auth0rize.signin') ? this.signin(req, res, next) : next();
+                }
+                //app.get('/auth0rize.sse', this.sse);
+                //app.post('/auth0rize.signin', this.signin);
+                //app.get('/auth0rize.sources', this.sources);
+                //app.post('/auth0rize.request', this.request);
+            ]
         }
 
         sse(req, res) {
             return this.channel.subscribe(req, res);
         }
 
-        apply(req, res) {
-            //call from authorize.core.api
-            let { token } = req.body;
+        async sources(req, res) {
+            let jwt = jsonwebtoken.sign({ api_key: this.api_key }, this.secret);
+                
+            let { data } = await this.auth0rize.post('/client.sources', { jwt });
 
-            this.channel.publish(req.body, token);
+            res.json(data.content);
+
+            return data;
         }
 
-        sources(req, res) {
-            //call authorize.core.api
+        async request(req, res) {
+            let { bots, link_type, meta } = req.body;
+
+            bots = bots.map(bot => {
+                let jwt = jsonwebtoken.sign({ bot_id: bot, link_type, meta, api_key: this.api_key }, this.secret);
+            
+                return this.auth0rize.post('/client.request', { jwt });
+            });
+            
+            bots = await Promise.all(bots);
+
+            bots = bots.map(bot => bot.data.content);
+
+            res.json(bots);
+            
+            return bots;
         }
 
-        create(req, res) {
-            //call authorize.core.api
+        message(req, res) {
+            let { contact, meta, token, source } = req.body;
+
+            let jwt = jsonwebtoken.sign({ contact, meta, token, source }, this.secret);
+
+            let response = this.onMessage ? this.onMessage({ contact, meta, token, source }) : 'Hello, man!';
+
+            res.end(response);
+
+            this.channel.publish(jwt, token);
+
+            return response;
         }
 
-        signIn(req, res) {
-            //call authorize.core.api
-            this.processSignIn && this.processSignIn();
+        signin(req, res, next) {
+            let { jwt } = req.body;
+
+            try {
+                let message = jsonwebtoken.verify(jwt, process.env.AUTH0RIZE_SECRET);
+
+                this.onSignIn ? this.onSignIn(req, res, message) : next();
+            }
+            catch(err) {
+                throw { code: 422, message: 'Client verification failed.' };
+            }
         }
     }
 
@@ -165,49 +110,68 @@ if(typeof(window) === 'undefined') {
 }
 else {
     //client side
-    class Auth0rize {
-        constructor({ base_url, onCreate, onSignIn }) {
-            this.base_url = base_url;
+    class Auth0rize extends EventTarget {
+        constructor({ url, onCreate, onSignIn }) {
+            super();
+            this.url = url;
             this.onCreate = onCreate;
             this.onSignIn = onSignIn;
+
+            if(window.EventSource) {
+                this.eventSource = new EventSource(`${this.url}/auth0rize.events`);
+            }
         }
 
-        async sources() {
-            let response = await fetch({
-                url: `${base_url}/auth0rize/sources`
-            });
+        async sources({ meta = {} } = {}) {
+            let url = `${this.url}/auth0rize.sources`;
+
+            let response = await fetch(url);
+            response = response ? await response.json() : [];
 
             return response.map(source => {
-                let { _id, name } = source;
+                let { _id, active, name, _class } = source;
 
                 return {
+                    _id,
+                    active,
                     name,
+                    _class,
                     request: async (link_type = 'web') => {
-                        let response = await fetch({
+                        let bots = [_id];
+
+                        let response = await fetch(`${this.url}/auth0rize.request`, {
                             method: 'POST',
-                            url: `${base_url}/auth0rize/create`,
-                            data: { _id, name, link_type }
+                            body: JSON.stringify({ bots, link_type, meta }),
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
                         });
+
+                        response = response ? await response.json() : {};
                         
-                        let { token } = response;
+                        let [{ token }] = response;
 
                         this.onCreate && this.onCreate(response); // return qr & link & token
-                        this.eventSource = void 0;
 
-                        if(window.EventSource) {
-                            this.eventSource = new EventSource(`${base_url}/auth0rize/sse`);
-                            
-                            eventSource.addEventListener(token, async e => {
-                                let response = await fetch({
-                                    method: 'POST',
-                                    url: `${base_url}/auth0rize/signin`,
-                                    data: { token }
-                                });
-
-                                this.onSignIn && this.onSignIn(response);
+                        this.eventSource && this.eventSource.addEventListener(token, async e => {
+                            let jwt = e.data;
+                            debugger
+                            let response = await fetch(`${this.url}/auth0rize.signin`, {
+                                method: 'POST',
+                                body: JSON.stringify({ jwt }),
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                }
                             });
-                        }
+    
+                            response = response ? await response.json() : {};
+                            debugger
+                            let event = new CustomEvent(token, { detail: response });
+
+                            this.dispatchEvent(event);
+                        });
                         
+                        return response;
                     }
                 }
             });
@@ -218,7 +182,3 @@ else {
 }
 
 module.exports = _class;
-
-/* module.exports = {
-    auth0rize
-} */
